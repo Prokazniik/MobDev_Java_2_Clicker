02fa638e1bdd283661a1ff7681564deea8d81dfb
Prokazniik <illidan.mlg@gmail.com>
2019-04-04 02:45:04 +0300

    с background

--- app/src/main/java/com/example/mobile_task_1_2_clicker/MainActivity.java
+++ app/src/main/java/com/example/mobile_task_1_2_clicker/MainActivity.java
@@ -1,23 +1,25 @@
 package com.example.mobile_task_1_2_clicker;
 
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextClock;
+import android.widget.ImageButton;
 import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
     TextView mainText;
     Button mainBtn;
-    int i;
-    Button subtarctBtn;
-    Button resetBtn;
-
-
+    Button mainBtn2;
+    Button mainBtn3;
+    ImageButton mainImg;
 
     private long score = 0;
+    String st;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,47 +30,75 @@ public class MainActivity extends AppCompatActivity {
         mainBtn = (Button) findViewById(R.id.button);
         mainBtn2 = (Button) findViewById(R.id.button2);
         mainBtn3 = (Button) findViewById(R.id.button3);
-
-
+        mainImg=(ImageButton) findViewById(R.id.imageButton);
 
         View.OnClickListener clickListener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 score ++;
-                String s = "Кликов: " + score;
+                if (Math.abs(score)<10 & 5>Math.abs(score) & Math.abs(score)>1 || Math.abs(score)<100 & Math.abs(score)>15 & 5>(Math.abs(score)%10) & (Math.abs(score)%10)>1)
+                {
+                    st="а";
+                }
+                else
+                {
+                    st=" ";
+                }
+                String s = "Вы нажали на кнопку: " + score + " раз" + st;
                 mainText.setText(s.toCharArray(),0, s.length());
             }
         };
 
         mainBtn.setOnClickListener(clickListener);
+
         View.OnClickListener clickListener2 = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                score ++;
-                String s = "Кликов: " + score;
+                score --;
+                if (Math.abs(score)<10 & 5>Math.abs(score) & Math.abs(score)>1 || Math.abs(score)<100 & Math.abs(score)>15 & 5>(Math.abs(score)%10) & (Math.abs(score)%10)>1)
+                {
+                    st="а";
+                }
+                else
+                {
+                    st=" ";
+                }
+                String s = "Вы нажали на кнопку: " + score + " раз" + st;
                 mainText.setText(s.toCharArray(),0, s.length());
             }
         };
+
         mainBtn2.setOnClickListener(clickListener2);
 
         View.OnClickListener clickListener3 = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                score ++;
-                String s = "Кликов: " + score;
+                score =0;
+                if (Math.abs(score)<10 & 5>Math.abs(score) & Math.abs(score)>1 || Math.abs(score)<100 & Math.abs(score)>15 & 5>(Math.abs(score)%10) & (Math.abs(score)%10)>1)
+                {
+                    st="а";
+                }
+                else
+                {
+                    st=" ";
+                }
+                String s = "Вы нажали на кнопку: " + score + " раз" + st;
                 mainText.setText(s.toCharArray(),0, s.length());
             }
         };
+
         mainBtn3.setOnClickListener(clickListener3);
 
         View.OnClickListener clickListener4 = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                score ++;
-                String s = "Кликов: " + score;
+                score =777;
+                String s = "АМР" + score;
                 mainText.setText(s.toCharArray(),0, s.length());
             }
-            };
+        };
+
+        mainImg.setOnClickListener(clickListener4);
 
     }
 }
